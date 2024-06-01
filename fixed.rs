bd7e74b4cd221ae0355fd76e8e1fc14ac06abc99
bartolobrians <80990925+bartolobrians@users.noreply.github.com>
2024-06-01 18:15:28 +0800

    Update fixed.rs

--- fixed.rs
+++ fixed.rs
@@ -1,6 +1,11 @@
 pub struct FixedPredictor;
 
 impl FixedPredictor {
+    /// Get order that yields the least sum of residuals
+    /// 
+    /// The predictor orders are from 0 to 4 inclusive and is retrieved
+    /// by finding the predictor that yields the *minimum* absolute
+    /// sum of residuals for the given `data` and derived predictor.
     pub fn best_predictor_order(data: &Vec<i64>) -> Option<u8> {
         let orders = 0..=4;
         let mut best_order = None;
@@ -19,6 +24,24 @@ impl FixedPredictor {
         best_order
     }
 
+    /// Get residuals of a fixed predictor order 
+    /// 
+    /// The predictor orders are from 0 to 4 inclusive and corresponds
+    /// to one of the five "fixed" predictor orders written in the FLAC
+    /// specification. The predictor orders are defined as follows:
+    /// 
+    /// 0: r[i] = 0
+    /// 1: r[i] = data[i - 1]
+    /// 2: r[i] = 2 * data[i - 1] - data[i - 2]
+    /// 3: r[i] = 3 * data[i - 1] - 3 * data[i - 2] + data[i - 3]
+    /// 4: r[i] = 4 * data[i - 1] - 6 * data[i - 2] + 4 * data[i - 3] - data[i - 4]
+    /// 
+    /// This function returns a vector with each element containing data[i] - r[i].
+    /// 
+    /// # Errors
+    /// `None` is returned if an error occurs in the function. This includes whether
+    /// the predictor order provided is not within 0 and 4 inclusive and whether the
+    /// size of `data` is less than the predictor order.
     pub fn get_residuals(data: &Vec<i64>, predictor_order: u8) -> Option<Vec<i64>> {
         if predictor_order > 4 || data.len() < (predictor_order as usize) {
             return None;
@@ -31,31 +54,37 @@ impl FixedPredictor {
                 0 => 0,
                 1 => {
                     if i == 0 {
-                        return None;
+                        0
+                    } else {
+                        data[i - 1]
                     }
-                    data[i - 1]
                 },
                 2 => {
                     if i < 2 {
-                        return None;
+                        0
+                    } else {
+                        2 * data[i - 1] - data[i - 2]
                     }
-                    2 * data[i - 1] - data[i - 2]
                 },
                 3 => {
                     if i < 3 {
-                        return None;
+                        0
+                    } else {
+                        3 * data[i - 1] - 3 * data[i - 2] + data[i - 3]
                     }
-                    3 * data[i - 1] - 3 * data[i - 2] + data[i - 3]
                 },
                 4 => {
                     if i < 4 {
-                        return None;
+                        0
+                    } else {
+                        4 * data[i - 1] - 6 * data[i - 2] + 4 * data[i - 3] - data[i - 4]
                     }
-                    4 * data[i - 1] - 6 * data[i - 2] + 4 * data[i - 3] - data[i - 4]
                 },
                 _ => unreachable!(),
             };
-            residuals.push(data[i] - r_i);
+            if i >= predictor_order as usize {
+                residuals.push(data[i] - r_i);
+            }
         }
 
         Some(residuals)
